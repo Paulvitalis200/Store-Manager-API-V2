a7a49eb8fb0ae344dc31ee0c6a6658be56213378
Paulstar200 <vitalispaul48@live.com>
2018-10-28 01:27:58 +0300

    [Chore #161475639] Database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint
 from flask_jwt_extended import JWTManager
+from app.db_con import db_connection, create_tables
 
 
 def create_app(config):
@@ -8,6 +9,9 @@ def create_app(config):
     from instance.config import app_config
     app.config.from_object(app_config[config])
     app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+
+    db_connection()
+    create_tables()
 
     jwt = JWTManager(app)
 
---
+++ app/db_con.py
@@ -0,0 +1,59 @@
+import psycopg2
+from psycopg2 import Error
+
+
+def db_connection():
+    try:
+        conn = psycopg2.connect(
+            user="postgres",
+            password="manu2012",
+            host="127.0.0.1",
+            port="5432",
+            database="store_manager_api"
+        )
+        return conn
+    except (Exception, psycopg2.DatabaseError) as error:
+        return ('Failed to connect', error)
+
+
+def create_tables():
+    conn = db_connection()
+    curr = conn.cursor()
+    queries = tables()
+
+    for query in queries:
+        curr.execute(query)
+    curr.close()
+    conn.commit()
+
+
+def tables():
+    products_table = """
+                    CREATE TABLE IF NOT EXISTS products (
+                    id serial PRIMARY KEY NOT NULL,
+                    name text NOT NULL,
+                    price integer NOT NULL,
+                    quantity integer NOT NULL
+                    )
+                    """
+    sales_table = """
+                    CREATE TABLE IF NOT EXISTS sales (
+                    id serial PRIMARY KEY NOT NULL,
+                    description text NOT NULL,
+                    items text NOT NULL
+                    )
+                 """
+    users_table = """
+                    CREATE TABLE IF NOT EXISTS users (
+                    id serial PRIMARY KEY NOT NULL,
+                    username text NOT NULL,
+                    email text NOT NULL,
+                    password text NOT NULL
+                    )
+                """
+    queries = [products_table, sales_table, users_table]
+    return queries
+
+
+def destroy_tables():
+    pass
