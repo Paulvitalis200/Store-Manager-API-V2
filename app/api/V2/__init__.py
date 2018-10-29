8a777f1e00acd1884804fb9f1227b783c06694e1
Paulstar200 <vitalispaul48@live.com>
2018-10-30 00:09:18 +0300

    [Feature #161502724] Create UserLogin Resource

--- app/api/V2/__init__.py
+++ app/api/V2/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 from .views.products import Products
-from .views.users import UserRegistration
+from .views.users import UserRegistration, UserLogin
 
 my_apis = Blueprint("resources.api", __name__, url_prefix='/api/v2')
 api = Api(my_apis)
@@ -23,4 +23,10 @@ api.add_resource(
     UserRegistration,
     "/register",
     endpoint="register"
+)
+
+api.add_resource(
+    UserLogin,
+    "/login",
+    endpoint="login"
 )
--- app/api/V2/views/users.py
+++ app/api/V2/views/users.py
@@ -31,3 +31,32 @@ class UserRegistration(Resource):
         except Exception as e:
             print(e)
             return {"message": "Something is amiss"}
+
+
+class UserLogin(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', required=True, help='Email cannot be blank')
+    parser.add_argument('password', required=True, help='Password cannot be blank', type=str)
+
+    def post(self):
+        args = UserLogin.parser.parse_args()
+        password = args.get('password')
+        email = args.get('email').strip()
+
+        # generate a hash for the password
+        hash = UserModel.generate_hash(password)
+
+        # check if user by the email exists
+        current_user = UserModel.find_by_email(email)
+
+        # compare user's password and the hashed password
+        if UserModel.verify_hash(password, hash) and current_user != None:
+            access_token = create_access_token(identity=email, expires_delta=datetime.timedelta(days=5))
+            return {
+                'message': 'Log in successful!',
+                'access_token': access_token
+            }, 201
+        else:
+            return {
+                'message': 'That user does not exist or Incorrect email or password. Try again'
+            }, 400
