5bfd3928c7f0082e6e2d7c641524b3c9b1147057
Paulstar200 <vitalispaul48@live.com>
2018-11-05 13:37:34 +0300

    [Bug #161618639] Fix environment variable settings

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from flask import redirect
 from app import create_app
 
 config = os.getenv('APP_SETTINGS')
-app = create_app('development')
+print(config)
+app = create_app(config)
 
 
 @app.route("/documentation")
