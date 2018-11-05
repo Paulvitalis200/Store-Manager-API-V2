5b2f761793c949fc70c147583c2ceed8f837f423
Paulstar200 <vitalispaul48@live.com>
2018-11-05 19:55:33 +0300

    [Bug #161618639] Add tests for sales

--- app/tests/test_sales.py
+++ app/tests/test_sales.py
@@ -23,8 +23,7 @@ class ProductTest(unittest.TestCase):
     }
     self.empty_name = {
         "name": "",
-        "quantity": 20,
-
+        "quantity": 20
     }
 
     self.empty_price = {
@@ -36,17 +35,38 @@ class ProductTest(unittest.TestCase):
     res = self.client.post(
         '/api/v2/auth/login',
         data=json.dumps(
+            dict(email="vitalispaul48@live.com", password="manu2012")
+        ),
+        content_type='application/json')
+    return json.loads(res.get_data().decode("UTF-8"))['access_token']
+
+  def login_attendant(self):
+    res1 = self.client.post(
+        '/api/v2/auth/signup',
+        data=json.dumps(
+            dict(
+                username="paul200",
+                email="vitalman@gmail.com",
+                password="manu2012",
+                role="attendant"
+            )
+        ), headers=dict(Authorization="Bearer " + self.login()),
+        content_type='application/json')
+    res = self.client.post(
+        '/api/v2/auth/login',
+        data=json.dumps(
             dict(email="vitalman@gmail.com", password="manu2012")
         ),
         content_type='application/json')
     return json.loads(res.get_data().decode("UTF-8"))['access_token']
 
   def test_empty_name(self):
+
     res = self.client.post(POST_PRODUCT_URL,
                            content_type='application/json',
                            data=json.dumps(self.empty_name),
                            headers=dict(
-                               Authorization="Bearer " + self.login())
+                               Authorization="Bearer " + self.login_attendant())
                            )
     data = json.loads(res.get_data().decode("UTF-8"))
     self.assertEqual(data['message'], "Product does not exist")
